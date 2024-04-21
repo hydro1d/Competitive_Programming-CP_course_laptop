5c8c962f6a29e12eb6f99b11926c47f459fa8772
hydro1d <gghydroid@gmail.com>
2024-04-22 04:07:08 +0600

    22/4/24

---
+++ CP_course_laptop/Inferior Complexicty/1.cpp
@@ -0,0 +1,20 @@
+// custom header file banano. ".h diye"
+// defoine kora
+
+#include<bits/stdc++.h>
+#define add(a,b) a+b
+#define mul(a,b) a*b
+#define square(a) a*a
+
+//important oneke use kore
+#define ll long long 
+using namespace std;
+int main()
+{
+	int p = 10; int q =20;
+	cout<<p+q<<endl;
+	cout<<mul(3,4)<<endl;
+	cout<<square(3)<<endl;
+	cout<<square(1+2)<<endl;
+	return 0;
+}
---
+++ CP_course_laptop/Inferior Complexicty/2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b,y;
+	a= 1; 
+	b= 5;
+
+	y= a^b;
+	cout<<y<<endl;
+	cout<<(1|6)<<endl;
+	cout<< (5 & 7)<<endl;
+	cout<<(~-10)<<endl; // bitwise e operator deya lage
+
+//left shift
+	int x=3;
+	cout<<"left shift "<<(x<<1)<<endl;// 3 er binary theke left
+	//er dike ekbar shift holo tahole hoi 6
+//right shift
+	int z= 3;
+	cout<<"right shift "<<(z>>1)<<endl;
+	return 0;
+
+}
+//note that
+// x<<1 = x*2
+// x<<2= x*2*2
